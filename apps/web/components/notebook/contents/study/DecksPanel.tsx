788cff0708222091090ddf6322a5609b25513c9d
dongmingyang <dongmingyang2007@qq.com>
2026-04-16 10:18:03 -0700

    refactor(web): StudyWindow becomes a tab shell; Assets body lifted out

---
+++ apps/web/components/notebook/contents/study/DecksPanel.tsx
@@ -0,0 +1,3 @@
+"use client";
+interface Props { notebookId: string; onStartReview: (deckId: string) => void; }
+export default function DecksPanel(_: Props) { return <div>DecksPanel (TODO)</div>; }
---
+++ apps/web/components/notebook/contents/study/ReviewSession.tsx
@@ -0,0 +1,3 @@
+"use client";
+interface Props { deckId: string; onExit: () => void; }
+export default function ReviewSession(_: Props) { return <div>ReviewSession (TODO)</div>; }
